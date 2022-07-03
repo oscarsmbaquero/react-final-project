d644c4dca86a4cc404f030a19656b85443f1986f
oscarsmbaquero <94797307+oscarsmbaquero@users.noreply.github.com>
2022-07-03 11:53:42 +0200

    Create project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+     <h1> Final Project </h1>
+     <p>Vamos chavales</p>
+    </div>
+  );
+}
+
+export default App;
