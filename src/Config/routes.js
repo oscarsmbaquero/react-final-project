4d57d2f9b7d90a4af4aa38249b054f73f74737e8
dosorio55 <dosorio55@gmail.com>
2022-07-03 17:37:17 +0200

    agregadas rutas y funcionando, header con links al chat y al home

--- src/Config/routes.js
+++ src/Config/routes.js
@@ -1,38 +1,14 @@
-
+import Chat from "../components/Chat/Chat";
+import Home from "../components/Home/Home";
 
 const routes = [
   {
-   
+    path: "/",
+    element: <Home />,
   },
   {
-   
-  },
-  {
-    
-  },
-  {
-    
-  },
-  {
-    
-  },
-  {
-    
-  },
-  {
-   
-  },
-  {
-    
-  },
-  {
-    
-  },
-  {
-    
-  },
-  {
-    
+    path: "/Chat",
+    element: <Chat />,
   },
 ];
 
