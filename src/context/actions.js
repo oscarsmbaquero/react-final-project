43fc6bb40b84af1d78c865b7d6ad967abd336246
dosorio55 <dosorio55@gmail.com>
2022-07-22 20:05:48 +0200

    fixed context

--- src/context/actions.js
+++ src/context/actions.js
@@ -8,13 +8,11 @@ const requestOptions = (sendData) => {
     }
 };
 
-
 const fetchUserToDatabase = async (urlPetition, reqData) => {
     const res = await fetch(`${BASE_URL}/${urlPetition}`, requestOptions(reqData));
     const data = await res.json();
     return data;
 }
-
 
 export const loginUser = async (dispatch, loginData) => {
     localStorage.removeItem('currentUser');
@@ -22,15 +20,12 @@ export const loginUser = async (dispatch, loginData) => {
     try {
         dispatch({ type: "REQ_LOGIN" });
 
-
         const data = await fetchUserToDatabase("users/login", loginData)
-
 
         if (data.data) {
             dispatch({ type: "LOGIN_OK", payload: data.data });
             localStorage.setItem('currentUser', JSON.stringify(data.data));
         }
-
     } catch (error) {
         dispatch({ type: 'LOGIN_FAIL', error: error });
 
@@ -38,18 +33,15 @@ export const loginUser = async (dispatch, loginData) => {
 
 };
 
-
 export const registerUser = async (dispatch, registerData) => {
     localStorage.removeItem('currentUser');
 
     try {
         dispatch({ type: "REQ_REGISTER" })
-
         const data = await fetchUserToDatabase("users", registerData);
 
         if (data.status === 201) {
             const dataLogin = await fetchUserToDatabase("users/login", registerData)
-
             dispatch({ type: "LOGIN_OK", payload: dataLogin.data })
             localStorage.setItem('currentUser', JSON.stringify(dataLogin.data));
         }
@@ -57,9 +49,7 @@ export const registerUser = async (dispatch, registerData) => {
         dispatch({ type: 'LOGIN_FAIL', error: error });
     }
 
-
 };
-
 
 export const logout = async (dispatch) => {
     dispatch({ type: 'LOGOUT' });
