ac86654263f447f7bcb5a462075a471d2c4fe4cd
dosorio55 <dosorio55@gmail.com>
2022-07-23 13:57:51 +0200

    arreglado el error del edit profile

--- src/pages/Chat/Components/CurrentUser.jsx
+++ src/pages/Chat/Components/CurrentUser.jsx
@@ -1,14 +1,34 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { BASE_URL } from '../../../assets/ApiRoutes';
 import { useGetAuth } from '../../../context'
 import './CurrentUser.scss'
 
 const CurrentUser = () => {
+    const [currentUser, setCurrentUser] = useState("");
 
     const loggedUser = useGetAuth()
 
+    useEffect(() => {
+        fetch(`${BASE_URL}/users/${loggedUser.id}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            }
+        })
+            .then(res => res.json())
+            .then(data => setCurrentUser(data))
+    }, [])
+
+    console.log(currentUser);
+
     return (
         <div className='currenteUser'>
-            CurrentUser
+            <div>
+                {/* <img src={currentUser.image} alt="" /> */}
+            </div>
+            <p>
+                {currentUser.name}
+            </p>
         </div>
     )
 }
