8f13c31d895ab00a6c275e7c70097ace94e012be
Gabi <gabilondo_87@hotmail.com>
2022-07-24 12:59:31 +0200

    ad and delete contacts

--- src/pages/User/UserList/UserList.jsx
+++ src/pages/User/UserList/UserList.jsx
@@ -5,35 +5,47 @@ import './UserList.scss';
 import { useGetAuth } from "../../../context/context";
 import Swal from 'sweetalert2';
 import { BASE_URL } from '../../../assets/ApiRoutes';
+import User from './User';
+
 
 
 const UserList = () => {
 
   let [users, setUsers] = useState([]);
   let [userContact, setuserContact] = useState([]);
+  
   const [keyword, setKeyword] = useState("");//estado para almacenar y setear las entradas de teclado para el input
   const userLogged = useGetAuth();
 
+
   
-  const addContact = (e,id) =>{
-    
-    fetch(`${BASE_URL}/users/addContact`, {
-      method: 'PUT',
-      headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${userLogged.token}`
-      },
-      body: JSON.stringify({
-          
-          contactId: id
-      })
-     })
-      .then(res => res.json())
-      .then(data => setuserContact(data))
-       Swal.fire("Agregado corectamente", "success");
+  useEffect(() => {
 
-  }
+    const fetchData = async () => {
+        try {
+            const data = await fetch(`${BASE_URL}/users/contacts`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    Authorization: `Bearer ${userLogged.token}`
+                }
+            });
 
+            const jsonData = await data.json();
+            setuserContact(jsonData.data.contacts);
+            
+
+        } catch (error) {
+            // logout(dispatch)
+            // navigate('/users/login')
+            return console.log(error, 'vaya, ha habido un error')
+        }
+    }
+
+    fetchData()
+}, [userLogged.token]);
+  
+console.log(userContact)
   useEffect(() => {
     fetch(`${BASE_URL}/users`)
       .then(response => response.json())
@@ -46,38 +58,23 @@ const UserList = () => {
     e.preventDefault();
     setKeyword(e.target.value.toLowerCase());
   };
- 
+
   /*Con el valor introducido en el inpute del buscador filtramos los trabajos almacenaos en jobs(filtrado por puesto de trabajo o name)
     previo paso a minusculas*/
   const filteredUsers = users.filter((user) =>
-  // users.job.toLowerCase().includes(keyword) ||
-  user.name.toLowerCase().includes(keyword)
-  //console.log(user.name)
+    // users.job.toLowerCase().includes(keyword) ||
+    user.name.toLowerCase().includes(keyword)
+    //console.log(user.name)
   );
+
 
   return (
     <>
       <SearchInput placeholder="Filter by job or user " onChange={onInputChange} />
       <section className="userList">
-        {filteredUsers.map((post, key) => (
-          <div key={key} className="userList__div">
-          <img className="userList__photo" src={post.image} alt={post.name}/>
-            <div className='userList__div1' >
-           
-              <h1 className='userList__h1' >Name: {post.name}</h1>
-              <h2 className='userList__h2' >Surname: {post.surname}</h2>
-              <h3 className='userList__h3' >Email: {post.email}</h3>
-              {/* <p className='userList__p' >Direction: {post.direction}</p>
-              <p className='userList__p' >Job: {post.job}</p> */}
-            </div>
-            {userLogged.id !== post._id ?
-              <button className='userList__btn'  onClick = {(e) => addContact (e,post._id)}>Añadir Contacto</button>
-              :''
-            }
-            <Link to={`/users/${post._id}`}>
-              <button className='userList__btn' >Show More</button>
-            </Link>
-          </div>
+        {filteredUsers.map((post) => (
+          userLogged.id !== post._id &&
+          <User key={post._id} post={post} contacts={userContact}/>
         ))}
       </section>
     </>
