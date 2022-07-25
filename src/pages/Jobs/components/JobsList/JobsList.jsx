2a6def8fb07d149831011bee928b226459ceab0f
Manuel Alcázar <manuzz3112@gmail.com>
2022-07-25 17:06:49 +0200

    prueba de jobs

--- src/pages/Jobs/components/JobsList/JobsList.jsx
+++ src/pages/Jobs/components/JobsList/JobsList.jsx
@@ -12,7 +12,6 @@ const JobsList = ({ job, setSelectedJob }) => {
                 <div className='jobList__div1'>
                     <h1 className='jobList__h1'>{job.name}</h1>
                     <h3 className='jobList__h3'>Descripción: {job.description}</h3>
-                    
                 </div>
             </div>
         </div>
