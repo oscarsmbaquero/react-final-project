172af512baafe42a8b6cd51e41b18237302ce213
dosorio55 <dosorio55@gmail.com>
2022-07-25 16:02:02 +0200

    solving things

--- src/pages/User/Profile/Profile.jsx
+++ src/pages/User/Profile/Profile.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useReducer, useState } from 'react';
 import Swal from "sweetalert2";
 import { useNavigate } from "react-router-dom";
 import { BASE_URL } from '../../../assets/ApiRoutes';
@@ -10,10 +10,60 @@ import EditProfile from './Components/EditProfile';
 import ShowCandidatures from './Components/ShowCandidatures';
 import ShowContact from './Components/ShowContact';
 import GetRecruiterJobs from './Components/GetRecruiterJobs';
+import Notifications from './Components/Notifications';
+
+const tabsInitState = {
+  infomation: false,
+  contacts: false,
+  jobsList: false,
+  notifications: false
+};
+
+const tabsReducer = (currentState, action) => {
+
+  switch (action.type) {
+    case "INFORMATION":
+      return {
+        ...action.payload,
+        infomation: true
+      };
+    case "CONTACTS":
+      return {
+        ...action.payload,
+        contacts: true
+      };
+    case "JOBS":
+
+      return {
+        ...action.payload,
+        jobsList: true
+      };
+    case "NOTIFICATIONS":
+
+      return {
+        ...action.payload,
+        notifications: true
+      };
+    default:
+      break;
+  }
+
+};
 
 const Profile = () => {
 
-  let navigate = useNavigate();
+  if (true) {
+    
+  }
+
+  const [tabs, dispatch] = useReducer(tabsReducer, tabsInitState);
+
+  const infoTab = () => dispatch({ type: "INFORMATION", payload: tabsInitState });
+  const contactsTab = () => dispatch({ type: "CONTACTS" });
+  const jobsTab = () => dispatch({ type: "JOBS" });
+  const notificationsTab = () => dispatch({ type: "NOTIFICATIONS" });
+
+  const navigate = useNavigate();
 
   const [profile, SetProfile] = useState();
   const [edit, setEdit] = useState();
@@ -43,9 +93,9 @@ const Profile = () => {
     })
   };
 
-  const showContacts = async () => {
-    // const thisClicked = e.currentTarget;
-    // thisClicked.innerText ="Mostrando"  ;
+  const showContacts = async (e) => {
+    const thisClicked = e.currentTarget;
+    thisClicked.innerText = "Mostrando";
     const data = await fetch(`${BASE_URL}/users/contacts`, {
       method: 'GET',
       headers: {
@@ -91,7 +141,6 @@ const Profile = () => {
       .then(data => SetProfile(data))
   }, [userLogged.id]);
 
-
   return (
     <section className='detail'>
       {!profile ? <p>Cargando...</p> : <>
@@ -105,23 +154,49 @@ const Profile = () => {
           </div>
         </div>
 
-
         <div className="edits">
           <div className='edits__imptBtn'>
 
-            <button className="edits__button Info" onClick={() => setEdit(userLogged.id)} >Añadir Info</button>
-            {edit === userLogged.id && <EditProfile editProfile={profile} userLogged={userLogged} />}
-            <button className="edits__button Show" onClick={showContacts} >Mostar Contactos</button>
-            {userLogged.rol === 'User' ?
-              <button className='edits__buttonActive' onClick={showCandidatures} >Candidaturas Activas</button>
-              : <button className='edits__buttonActive' onClick={getRecruiterJobs} >Candidaturas Abiertas</button>
-            }
-            <ShowContact contacts={contacts} />
-            {userLogged.rol === 'User' ?
-              <ShowCandidatures contacts={contacts} />
+            <ul className='edits__navBar'>
+              <li onClick={infoTab}>Information</li>
+              <li onClick={contactsTab}>Contacts</li>
+              <li onClick={jobsTab}>Applied Jobs</li>
+              <li onClick={notificationsTab}>Notifications</li>
+            </ul>
+
+            {/* //##################### profile information */}
+            {tabs.infomation &&
+              <>
+                <button className="edits__button Info" onClick={() => setEdit(userLogged.id)} >Edit Info</button>
+                {edit === userLogged.id && <EditProfile editProfile={profile} userLogged={userLogged} />}
+              </>}
+
+            {/* //##################### Show contacts */}
+
+            {tabs.contacts &&
+              <>
+                <button className="edits__button Show" onClick={showContacts} >Mostar Contactos</button>
+                <ShowContact contacts={contacts} />
+              </>}
+
+            {/* //##################### Jobs */}
+
+
+            {tabs.jobsList &&
+              userLogged.rol === 'User' ?
+              <>
+                <button className='edits__buttonActive' onClick={showCandidatures} >Candidaturas Activas</button>
+                <ShowCandidatures contacts={contacts} />
+              </>
               :
-              <GetRecruiterJobs recruiterJobs={recruiterJobs} />
+              <>
+                <button className='edits__buttonActive' onClick={getRecruiterJobs} >Candidaturas Abiertas</button>
+                <GetRecruiterJobs recruiterJobs={recruiterJobs} />
+              </>
             }
+            {/* //##################### notifications */}
+            {tabs.notifications && <Notifications />}
+
           </div>
           <button className='Delete' onClick={(e) => deleteProfile(e, userLogged.id)} >Eliminar Perfil</button>
 
