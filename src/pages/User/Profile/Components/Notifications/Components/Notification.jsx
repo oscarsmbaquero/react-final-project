7d00695f51e7fe624b29fdf8b53aa7be3d701183
dosorio55 <dosorio55@gmail.com>
2022-07-26 15:21:47 +0200

    notifications casi ready

--- src/pages/User/Profile/Components/Notifications/Components/Notification.jsx
+++ src/pages/User/Profile/Components/Notifications/Components/Notification.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from "react-router-dom";
+
 import Swal from 'sweetalert2';
 import { BASE_URL } from '../../../../../../assets/ApiRoutes';
 import { defaultProfileImage } from '../../../../../../assets/images/imagesLink';
@@ -6,58 +8,64 @@ import { useGetAuth } from '../../../../../../context';
 
 const Notification = ({ notification }) => {
 
-  console.log(notification)
+  const [btnState, setBtnState] = useState("not seen");
 
   const loggedUser = useGetAuth();
 
   const handleButton = (event) => {
-    let notificationStatus;
-    event.target.innerText === "Accept" ? notificationStatus = "Accept"
-      : notificationStatus = "Reject"
-    fetch(`${BASE_URL}/jobs/add-user/`, {
+    console.log(event.target.innerText);
+    fetch(`${BASE_URL}/notifications/updateNotifications`, {
       method: 'PUT',
       headers: {
         'Content-Type': 'application/json',
         Authorization: `Bearer ${loggedUser.token}`
       },
       body: JSON.stringify({
-        type: notificationStatus,
-        // to:
-        // jobId:
-        //notificationId: notification._id
+        type: event.target.innerText,
+        to: notification.from._id,
+        jobId: notification.jobId,
+        notificationId: notification._id
       })
     }).then(res => {
       if (res.status === 200) {
         Swal.fire("Se ha actualizado correctamente");
+        setBtnState(event.target.innerText)
       } else {
-        // navigate('/users/login')
+        console.log("ha habido un error");
       }
     }).catch((error) => {
+      console.log(error);
       console.error(error);
     })
   };
 
-  // const notificationStatus = (notification) => notification.view_status === "Accept" ? <p>Accepted</p> : <p>refected</p>
+  const notificationStatus = () => notification.view_status === "Accept" || btnState === "Accept"
+    ? <p className='notificationsList__text'>Accepted</p>
+    : <p className='notificationsList__text'>Rejected</p>;
 
   return (
     <div className='notificationsList__card'>
       <div className='notificationsList__item'>
         <img className='notificationsList__img' src={notification.from.image || defaultProfileImage} alt={notification.from.name} />
       </div>
-      <div className='notificationsList__item'>
+      <Link to={`/users/${notification.from._id}`} className='notificationsList__item'>
         <p className="notificationsList__text">{notification.from.name}</p>
-      </div>
+      </Link>
       <div className='notificationsList__item'>
-        <p className="notificationsList__text">has applied to THIS job</p>
+        <p className="notificationsList__text">has applied to {notification.jobId.name} job</p>
       </div>
 
-      {/* {notification.view_status === "not seen" ? */}
-      <>
-        <button className='notificationsList__btn' onClick={handleButton}>Reject</button>
-        <button className='notificationsList__btn' onClick={handleButton}>Accept</button>
-      </>
-      {/* : notificationStatus() */}
-      {/* } */}
+      {notification.view_status === "not seen" && btnState === "not seen" ?
+        <>
+          <button className='notificationsList__btn' onClick={handleButton}>Reject</button>
+          <button className='notificationsList__btn' onClick={handleButton}>Accept</button>
+        </>
+        : notificationStatus()
+      }
+      {notification.view_status === "Accept" || btnState === "Accept" ?
+        <div>
+          <button className='notificationsList__btn'>Go to chat</button>
+        </div> : ""}
     </div>
   )
 }
