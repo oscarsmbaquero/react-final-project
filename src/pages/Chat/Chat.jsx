81e6d7e9ce82cf97391937e034e58163016644dc
dosorio55 <dosorio55@gmail.com>
2022-07-29 08:34:10 +0200

    botón chat solucionado en responsive

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -7,12 +7,28 @@ import { BASE_URL } from '../../assets/ApiRoutes';
 import { io } from "socket.io-client"
 import CurrentUser from './Components/CurrentUser';
 import { useParams } from 'react-router-dom';
+import { useWindowSize } from '../../utils/windowSize';
 
 export const SelectedChatContext = React.createContext();
 
 const Chat = () => {
     const [contacts, setContacts] = useState(undefined);
     const [selectedChat, setSelectedChat] = useState(undefined);
+    const [height, width] = useWindowSize();
+    const [showChat, setShowChat] = useState("hidden");
+
+    useEffect(() => {
+        // console.log("cambia");
+        if (width < 810) {
+            if (showChat === "hidden") {
+                setShowChat("chat")
+            }
+        } else if (width > 810) {
+            if (showChat === "chat" || showChat === "contacts") {
+                setShowChat("hidden")
+            }
+        }
+    }, [width])
 
     const { id: id_params } = useParams();
     const socket = useRef();
@@ -59,28 +75,34 @@ const Chat = () => {
         fetchData()
     }, [loggedUser.token]);
 
+    const handleShow = () => {
+        showChat === "chat" ? setShowChat("contacts") : setShowChat("chat")
+    }
+
     return (
         <section className='chatContainer'>
             <SelectedChatContext.Provider value={selectedChat}>
                 <div className="chat">
-                    <div className='chat__list'>
+                    {showChat === "contacts" || showChat === "hidden" ? <div className='chat__list'>
                         <div>
                             {contacts && contacts.map(contact =>
                                 <div key={contact.id}>
                                     <Contacts
                                         contact={contact}
                                         setSelectedChat={setSelectedChat}
+                                        setShowChat={setShowChat}
+                                        width={width}
                                     />
                                     <div className='chat__line'></div>
                                 </div>
                             )}
                         </div>
                         <CurrentUser />
-                    </div>
-                    <Messages socket={socket} />
+                    </div> : ""}
+                    {showChat === "chat" || showChat === "hidden" ? <Messages socket={socket} /> : ""}
                 </div>
             </SelectedChatContext.Provider>
-            <button className='chat__button'>Chatea</button>
+            <button className='chat__button' onClick={handleShow}>Contacts</button>
         </section>
     )
 }
